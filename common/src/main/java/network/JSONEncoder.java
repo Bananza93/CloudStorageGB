c7e53a6e4c1b58ee9f87e60bda956faab5fde450
Bananza93 <zubryaev.sergey@gmail.com>
2021-10-19 02:21:01 +0400

    Добавление межсетевого взаимодействия

--- common/src/main/java/network/JSONEncoder.java
+++ common/src/main/java/network/JSONEncoder.java
@@ -3,7 +3,6 @@ package network;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToMessageEncoder;
 
@@ -15,8 +14,6 @@ public class JSONEncoder extends MessageToMessageEncoder<Message> {
     @Override
     protected void encode(ChannelHandlerContext ctx, Message msg, List<Object> out) throws Exception {
         objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-//        ObjectWriter ow = objectMapper.writer().withDefaultPrettyPrinter();
-//        System.out.println(ow.writeValueAsString(msg));
         out.add(objectMapper.writeValueAsBytes(msg));
     }
 }
